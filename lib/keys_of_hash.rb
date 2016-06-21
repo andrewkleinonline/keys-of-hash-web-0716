b7708b9f1b047c274b1ec2c5a98b2023e8b09c74
quarthex15 <andrew.klein.j@gmail.com>
2016-06-20 22:22:13 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*key_search)
+    # code goes here    
+    
+    matching_keys = self.select {|key, val| key_search.include?(val)}
+    matching_keys = matching_keys.keys
   end
 end
